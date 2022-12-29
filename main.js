71ea7174973d3141639665cd6b31ff8ebe1b34ab
tuugii0719 <116425267+tuugii0719@users.noreply.github.com>
2022-12-30 00:31:16 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-const areaWidth = 30
-const areaHeight = 30
+const areaWidth = 10
+const areaHeight = 10
 const scale = 20
-const speed = 2000
-let food = { x: 0 ,
-    y: 0}
+const speed = 100
+let score = 0
+let food = {x : 0, y : 0} 
+let posLast={x:0,y:0}
    
 
 const area = document.getElementsByClassName('area')[0];
 area.style.width = `${areaWidth * scale}px`;
 area.style.height = `${areaHeight * scale}px`;
-let direction = 'left'
+let direction = 'right'
 let positions = [
-    { x: 1, y: 1 },
     { x: 2, y: 1 },
-    { x: 3, y: 1 },
-    { x: 4, y: 1 }
+    { x: 1, y: 1 },
+    { x: 0, y: 1 }
 ]
 console.log(positions)
 document.addEventListener('keydown', (event) => {
+    console.log(event.key)
     switch (event.key) {
-        case 'ArrowdDown':
+        case 'ArrowDown':
             changeDirection('down');
             break;
         case 'ArrowUp':
             changeDirection('up');
             break;
-        case 'ArroLeft':
+        case 'ArrowLeft':
             changeDirection('left');
             break;
         case 'ArrowRight':
@@ -40,11 +41,11 @@ function changeDirection(value) {
 
 function generateFood() {
      food = {
-        x: Math.floor(Math.random() * areaWidth),
+    x: Math.floor(Math.random() * areaWidth),
     y: Math.floor(Math.random() * areaHeight)
     }
+    console.log(food.x)
 }
-
 function goLeft() {
     let newPositions = [];
     newPositions.push ({
@@ -57,7 +58,47 @@ function goLeft() {
     }
     positions = newPositions
 }
-setInterval(() => {
+function goUp() {
+    let newPositions = [];
+    newPositions.push ({
+       y: positions[0].y === 0 ?  areaHeight - 1 : positions[0].y -1 ,
+       x: positions[0].x ,
+    })
+    
+    for (i = 0; i < positions.length - 1; i++) {
+        newPositions.push(positions[i])
+    }
+    positions = newPositions
+}
+function goDown() {
+    let newPositions = [];
+    newPositions.push ({
+       y: positions[0].y === areaHeight - 1  ? 0 : positions[0].y + 1 ,
+       x: positions[0].x,
+    })
+    
+    for (i = 0; i < positions.length - 1; i++) {
+       
+        newPositions.push(positions[i])
+    }
+    positions = newPositions
+}
+function goRight() {
+    let newPositions = [];
+    newPositions.push ({
+       y: positions[0].y,
+        x: positions[0].x === areaWidth - 1 ? 0 : positions[0].x + 1,
+    })
+    
+    for (i = 0; i < positions.length - 1; i++) {
+        newPositions.push(positions[i])
+    }
+    positions = newPositions
+}
+
+   
+
+let play = setInterval(() => {
     switch (direction) {
         case 'right':
             goRight();
@@ -66,20 +107,36 @@ setInterval(() => {
             goLeft();
             break;
         case 'up':
-            goLeft();
+            goUp();
             break;
         case 'down':
-            goLeft();
+            goDown();
             break;
     }
+    //hool ideh ued
+    if(food.y===positions[0].y && food.x===positions[0].x) {
+        score += 1,
+        generateFood()
+        console.log('hool idlee')
+        positions.push(positions[positions.length])
+    }
+    //uuriiguu murguhuiig shalgah ni
+    for (let index = 1; index < positions.length; index++) {
+       if(positions[0].x===positions[index].x && positions[0].y===positions[index].y ){
+        clearInterval(play)
+       }
+        
+    }
+    console.log(positions)
     const snakeHtml = positions
         .map((position) => `<div class="snake" style="width : ${scale}px; height : ${scale}px; top: ${position.y * scale}px; left: ${position.x * scale}px;"></div> `)
         .join('');
 
     const foodHtml = `<div class="food" style="width : ${scale}px; height : ${scale}px; top: ${food.y * scale}px; left: ${food.x * scale}px;"></div> `;
 
-    area.innerHTML = snakeHtml + foodHtml
-    console.log(positions)
-
+    area.innerHTML = foodHtml + snakeHtml
+    console.log(direction)
+    document.getElementsByClassName("score")[0].innerHTML=score
 }, speed);
+
 
